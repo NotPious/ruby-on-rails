b4dc615b5807d3518317a02855b77c334de5cd20
Eric <eric_young@epbfi.com>
2026-01-02 14:50:40 -0500

    Cleanup ignored files and commit .gitignore

--- backend/app/graphql/types/mutation_type.rb
+++
@@ -1,7 +0,0 @@
-module Types
-  class MutationType < Types::BaseObject
-    field :add_to_cart, mutation: Mutations::AddToCart
-    field :update_cart_item, mutation: Mutations::UpdateCartItem
-    field :create_order, mutation: Mutations::CreateOrder
-  end
-end
--- backend/db/migrate/20251231220128_create_carts.rb
+++
@@ -1,9 +0,0 @@
-class CreateCarts < ActiveRecord::Migration[8.1]
-  def change
-    create_table :carts do |t|
-      t.string :session_id
-
-      t.timestamps
-    end
-  end
-end
